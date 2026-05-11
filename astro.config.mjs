c302f35ab6b5934818079c2b470c730781607ce6
cuni10 <cunigramajo@hotmail.com>
2026-05-10 23:18:16 -0300

    Cambio a MP4 y config de server de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  server: {
+    host: true, // Esto expone el servidor a la red local siempre
+    port: 4321, // Puedes cambiar el puerto si prefieres otro
+  },
 });
